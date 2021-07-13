68b4f52abd352b77b7a1b945cead5d8a57f448fc
andreagorno <andreagorno98@gmail.com>
2021-07-13 17:23:43 +0200

    header completato

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,44 @@
+
 <header>
     <div class="top-header">
         <div class="container">
             <ul>
                 <li>
-                    <a href="#">dc power visa</a>
+                    <a href="#">dc power&#8480; visa &reg;</a>
                 </li>
                 <li>
-                    <a href="#">additional dc sites</a>
+                    <a href="#">additional dc sites <i class="fas fa-caret-down"></i></a>
                 </li>
             </ul>
         </div>
+    </div>
+
+    <div class="bottom-header">
+        <div class="container">
+            <div class="logo">
+                <img src="{{ asset("img/dc-logo.png") }}" alt="dc-logo">
+            </div>
+            <div class="pages">
+                <ul>
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#" class="active">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+                </ul>
+            </div>
+            <div class="search-bar">
+                <input type="search" placeholder="Search">
+            </div>
+        </div>
+    </div>
+
+    <div class="jumbotron">
+        
     </div>
 </header>
