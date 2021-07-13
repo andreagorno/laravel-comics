abc81c0062b554c1a1e84786439fbc47164220ff
andreagorno <andreagorno98@gmail.com>
2021-07-13 16:04:51 +0200

    prime modifiche

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="top-header">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">dc power visa</a>
+                </li>
+                <li>
+                    <a href="#">additional dc sites</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
